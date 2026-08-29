889e0958ddd9609820464e1cab9c9ac3b0615f57
najeeb dev <najeebkhoso057@gmail.com>
2026-08-29 17:40:40 +0500

    enchanced js for All button functionality

--- index.js
+++ index.js
@@ -9,12 +9,17 @@ const searchResult = document.querySelector(".searchResults");
 const popularMovies = document.querySelector(".popularMovies");
 const popularMoviesText = document.querySelector("#PM");
 const spellingCheck = document.querySelector("#spellingCheck");
+const genreBtns = document.querySelectorAll(".genreBtn");
+const Allbtn = document.getElementById("All");
+const AllMoviesSection = document.querySelector(".allMovies");
+const popularBtn = document.querySelector("#Popular");
 let data;
 let firstFourMovies;
 let poster_path;
 let imgUrl;
 let overView;
 let result;
+let MovieData;
 async function getObject() {
   const userSearch = searchBar.value;
   try {
@@ -25,12 +30,11 @@ async function getObject() {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
     data = await response.json();
-    console.log(data)
+    console.log(data);
     showResults();
     result = data.results;
-    if(result.length === 0) {
-      
-     popularMoviesText.innerText = "Coudld not find it , Check spelling."
+    if (result.length === 0) {
+      popularMoviesText.innerText = "Coudld not find it , Check spelling.";
     }
   } catch (error) {
     console.log(error);
@@ -38,18 +42,21 @@ async function getObject() {
 }
 function showResults() {
   popularMovies.classList.add("hidden");
+  AllMoviesSection.classList.add("hidden");
+  searchResult.classList.remove("hidden");
   searchResult.classList.add("flexContainer");
-  popularMoviesText.innerText = "Search Results"
-  popularMoviesText.classList.add("marginleft")
-  const noimg = "assets/no image.jpg"
+  popularMoviesText.innerText = "Search Results";
+  popularMoviesText.classList.add("marginleft");
+  const noimg = "assets/no image.jpg";
   firstFourMovies = data.results.slice(0, 5);
-  searchResult.innerHTML = firstFourMovies.map((movie) => {
-    if(movie.poster_path){
-     posterUrl = "https://image.tmdb.org/t/p/w500" + movie.poster_path; 
-    } else {
-      posterUrl = noimg;
-    }
-    return `
+  searchResult.innerHTML = firstFourMovies
+    .map((movie) => {
+      if (movie.poster_path) {
+        posterUrl = "https://image.tmdb.org/t/p/w500" + movie.poster_path;
+      } else {
+        posterUrl = noimg;
+      }
+      return `
 <article class="searchResultArticle"> 
 <img id="resultPoster" src="${posterUrl}" alt="">
 <h3 id="h3Title">${movie.title}</h3>
@@ -57,10 +64,69 @@ function showResults() {
   <span id="RD">${movie.release_date}</span>
 </article> 
   `;
-})
-  .join("")
+    })
+    .join("");
 }
-
 searchBtn.addEventListener("click", () => {
   getObject();
+});
+
+async function DisoverMovies() {
+  try {
+    const getObject = await fetch(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`,
+    );
+    if (!getObject.ok) {
+      throw new Error(`HTTP error! Status: ${getObject.status}`);
+    }
+    MovieData = await getObject.json();
+    showAllMovies();
+    console.log(MovieData);
+  } catch (error) {
+    console.log(error);
+  }
+}
+function showAllMovies() {
+  popularMovies.classList.add("hidden");
+  searchResult.classList.add("hidden");
+  AllMoviesSection.classList.remove("hidden");
+  popularMoviesText.innerText = "All";
+  popularMoviesText.style.marginLeft = "20px";
+  popularMoviesText.style.fontSize = "30px";
+  popularMoviesText.style.fontWeight = "400";
+  allMovies = MovieData.results.slice(0, 18);
+  AllMoviesSection.innerHTML = allMovies
+    .map((movie) => {
+      console.log(movie.title);
+      console.log(movie.genre_ids);
+      if (movie.poster_path) {
+        posterUrl = "https://image.tmdb.org/t/p/w500" + movie.poster_path;
+      } else {
+        posterUrl = noimg;
+      }
+      return `
+<article class="msBoxes"> 
+<img id="msPoster" src="${posterUrl}" alt="">
+<h3 id="h3Title">${movie.title}</h3>
+  <span id="RD">${movie.release_date}</span>
+</article> 
+  `;
+    })
+    .join("");
+}
+Allbtn.addEventListener("click", () => {
+  DisoverMovies();
+});
+// genreBtns.forEach((btn) => {
+// })
+popularBtn.addEventListener("click", () => {
+  AllMoviesSection.classList.add("hidden");
+  popularMoviesText.innerText = "Popular Movies";
+  popularMoviesText.style.marginLeft = "0px";
+  popularMoviesText.style.fontSize = "20px";
+  if (popularMovies.classList.contains("hidden")) {
+    popularMovies.classList.remove("hidden");
+  } else if (searchResult.classList.contains("visible")) {
+    searchResult.classList.add("hidden");
+  }
 });
