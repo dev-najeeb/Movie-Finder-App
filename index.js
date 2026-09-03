4501357474c72755461d808cfd05fbce2a376d7d
najeeb dev <najeebkhoso057@gmail.com>
2026-09-03 23:48:12 +0500

    - enchanced js for overview toggle logic

--- index.js
+++ index.js
@@ -1,50 +1,3 @@
-const API_KEY = "6c03d5b5d37483a4e164a2aa1c438cec";
-const searchBar = document.getElementById("searchBox");
-const searchBtn = document.getElementById("Search");
-const resultBox = document.getElementById("resultBox");
-const resultPoster = document.getElementById("resultBoxPoster");
-const resultSpan = document.getElementById("resultBoxSpan");
-const resultRD = document.getElementById("resultReleaseDate");
-const searchResult = document.querySelector("#searchMovies");
-const popularMovies = document.querySelector("#popularMovies");
-const MoviesText = document.querySelector("#MT");
-const spellingCheck = document.querySelector("#spellingCheck");
-const genreBtns = document.querySelectorAll(".genreBtn");
-const AllMoviesSection = document.querySelector("#allMovies");
-const allBtn = document.getElementById("All");
-const popularBtn = document.querySelector("#Popular");
-const actionBtn = document.querySelector("#Action");
-const adventureBtn = document.querySelector("#Adventure");
-const animationBtn = document.querySelector("#Animation");
-const comedyBtn = document.querySelector("#Comedy");
-const crimeBtn = document.querySelector("#Crime");
-const dramaBtn = document.querySelector("#Drama");
-const horrorBtn = document.querySelector("#Horror");
-const romanceBtn = document.querySelector("#Romance");
-const scifiBtn = document.querySelector("#Sci-fi");
-const thrillerBtn = document.querySelector("#Thriller");
-const actionMovieSection = document.querySelector("#actionMovies");
-const adventureMovieSection = document.querySelector("#adventureMovies");
-const animationMovieSection = document.querySelector("#animationMovies");
-const comedyMovieSection = document.querySelector("#comedyMovies");
-const crimeMovieSection = document.querySelector("#crimeMovies");
-const dramaMovieSection = document.querySelector("#dramaMovies");
-const horrorMovieSection = document.querySelector("#horrorMovies");
-const romanceMovieSection = document.querySelector("#romanceMovies");
-const scifiMovieSection = document.querySelector("#sci-fiMovies");
-const thrillerMovieSection = document.querySelector("#thrillerMovies");
-const genreSection = document.querySelectorAll(".genreSection");
-const genreBtn = document.querySelectorAll(".genreBtn");
-let data;
-let firstFourMovies;
-let poster_path;
-let imgUrl;
-let overView;
-let result;
-let MovieData;
-let allMovies;
-let actionMovies;
-
 async function fetchMovies(apiurl, amountofArray) {
   try {
     const response = await fetch(apiurl);
@@ -63,7 +16,7 @@ async function fetchMovies(apiurl, amountofArray) {
 function buildMovieHTML(movies) {
   const noimg = "assets/no image.jpg";
   return movies
-    .map((movie) => {
+  .map((movie , index) => {
       let posterUrl;
       if (movie.poster_path) {
         posterUrl = "https://image.tmdb.org/t/p/w500" + movie.poster_path;
@@ -71,11 +24,13 @@ function buildMovieHTML(movies) {
         posterUrl = noimg;
       }
       return `
-<article class="box"> 
-<img id="msPoster" src="${posterUrl}" alt="">
-<h3 id="h3Title">${movie.title}</h3>
-  <span id="RD">${movie.release_date}</span>
-  </article> 
+      <div class="box" style="--delay: ${index}"> 
+      <img class="boxImg" src="${posterUrl}" alt="">
+      <p class="movieOverview">${movie.overview}</p>
+      <h3 id="h3Title">${movie.title}</h3>
+      <span class="RD">${movie.release_date}</span>
+         <button class="showOverview">show overview</button>
+      </div> 
   `;
 })
 .join("");
@@ -117,9 +72,6 @@ async function DisoverMovies() {
 }
 function showAllMovies() {
   MoviesText.innerText = "All";
-  MoviesText.style.marginLeft = "20px";
-  MoviesText.style.fontSize = "30px";
-  MoviesText.style.fontWeight = "400";
   AllMoviesSection.innerHTML = buildMovieHTML(allMovies);
 }
 allBtn.addEventListener("click", () => {
@@ -127,8 +79,6 @@ allBtn.addEventListener("click", () => {
 });
 popularBtn.addEventListener("click", () => {
   MoviesText.innerText = "Popular Movies";
-  MoviesText.style.marginLeft = "0px";
-  MoviesText.style.fontSize = "20px";
   if (popularMovies.classList.contains("hidden")) {
     popularMovies.classList.remove("hidden");
   } else if (searchResult.classList.contains("visible")) {
@@ -180,3 +130,37 @@ scifiBtn.addEventListener("click",()=>{
   MoviesText.innerText = "Science Fiction Movies"
   showMovies(878 , scifiMovieSection)
 })
+setTimeout(() => {
+  
+  allSection.forEach((section) => {
+    section.addEventListener("click", (event) => {
+      const sectionBtn = event.target.closest("button");
+      if (sectionBtn) {
+        const SelectedSection = sectionBtn.closest(".genreSection")
+        const boxes = SelectedSection.querySelectorAll(".box")
+        const clickedbtnBox = sectionBtn.closest(".box");
+      
+        console.log(boxes)
+        
+        console.log("button was clicked");
+        const poster = clickedbtnBox.querySelector(".boxImg");
+        const title = clickedbtnBox.querySelector("#h3Title");
+        const rd = clickedbtnBox.querySelector(".RD");
+        const overview = clickedbtnBox.querySelector(".movieOverview")
+
+        console.log(clickedbtnBox);
+
+        clickedbtnBox.classList.add("clickedBox");
+        poster.classList.add("clickedboxImg");
+        rd.classList.add("clickedBoxRD");
+        title.classList.add("clickedBoxHeading");
+        overview.classList.add("visible")
+        overview.classList.add("Overview")
+        boxes.forEach((box) => {
+          box.classList.add("hidden");
+        });
+        clickedbtnBox.classList.remove("hidden");
+      }
+    });
+  });
+}, 500);
